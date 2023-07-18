e809642e8ea54ccddde2481d3d581e8f916a1b55
MadheshKumarVJ <vjmadhesh@testpress.in>
2023-07-18 17:29:39 +0530

    refactor code

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = config => {
     config.addPassthroughCopy("img");
     config.addPassthroughCopy("CNAME");
+    config.addPassthroughCopy("css/prism.css");
+    config.addPassthroughCopy("js/prism.js");
     return {
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
