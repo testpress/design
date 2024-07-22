f17f9602438f1d5da50717dcde4a6bc7aba2074d
MadheshKumarVJ <112080679+MadheshKumarVJ@users.noreply.github.com>
2024-07-22 12:08:52 +0530

    Add TpStreams player (#202)
    - Adding Tpstreams player to design page

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,18 @@ module.exports = config => {
     config.addPassthroughCopy("img");
     config.addPassthroughCopy("CNAME");
     config.addPassthroughCopy("css/prism.css");
+    config.addPassthroughCopy("css/settings.css");
+    config.addPassthroughCopy("css/video-js.css");
+    config.addPassthroughCopy("css/videojs-hls-quality-selector.css");
+    config.addPassthroughCopy("css/videojs-controls.css");
+    config.addPassthroughCopy("css/videojs-tps.css");
+    config.addPassthroughCopy("css/chapter-plugin.min.css");
+    config.addPassthroughCopy("js/settings.js");
+    config.addPassthroughCopy("js/video.js");
+    config.addPassthroughCopy("js/videojs-hls-quality-selector.js");
+    config.addPassthroughCopy("js/videojs-sprite-thumbnails.js");
     config.addPassthroughCopy("js/prism.js");
+    config.addPassthroughCopy("js/chapter-plugin.min.js");
     config.addPassthroughCopy("./tailwind.css")
     config.addPassthroughCopy('./tailwind.config.js')
     config.addPassthroughCopy("css/tailwind.css");
