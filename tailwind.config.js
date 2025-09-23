259e0559639877e9fe88f5d23b0b3e9a6391a902
Karthikeyantestpress <Karthikeyantestpress@users.noreply.github.com>
2025-09-23 11:41:32 +0000

    deploy: 0433b168071976358843775804d472fa4b65e96e

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,16 +17,16 @@ module.exports = {
       },
       colors: {
         primary: {
-          50: colors.blue[50],
-          100: colors.blue[100],
-          200: colors.blue[200],
-          300: colors.blue[300],
-          400: colors.blue[400],
-          500: colors.blue[500],
-          600: colors.blue[600],
-          700: colors.blue[700],
-          800: colors.blue[800],
-          900: colors.blue[900],
+          50: colors.emerald[50],
+          100: colors.emerald[100],
+          200: colors.emerald[200],
+          300: colors.emerald[300],
+          400: colors.emerald[400],
+          500: colors.emerald[500],
+          600: colors.emerald[600],
+          700: colors.emerald[700],
+          800: colors.emerald[800],
+          900: colors.emerald[900],
         }
       }
     },
