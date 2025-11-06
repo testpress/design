bc2ef7ec34a62305875010b817b4c8a90d569fa5
karthikeyan <karthikeyan@testpress.in>
2025-11-06 10:43:54 +0530

    update-code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,48 @@
 
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
+const patterns = [
+  "jigsaw", "overcast", "formalinvitation", "topography", "texture", "jupiter",
+  "architect", "cutout", "hideout", "graphpaper", "yyy", "squares",
+  "fallingtriangles", "pianoman", "piefactory", "dominos", "hexagons",
+  "charliebrown", "autumn", "temple", "stampcollection", "deathstar",
+  "churchonsunday", "ilikefood", "overlappinghexagons", "fourpointstars",
+  "bamboo", "bathroomfloor", "corkscrew", "happyintersection", "kiwi",
+  "lips", "lisbon", "randomshapes", "steelbeams", "tinycheckers", "xequals",
+  "anchorsaway", "bevelcircle", "brickwall", "fancyrectangles", "heavyrain",
+  "overlappingcircles", "plus", "roundedplusconnected", "volcanolamp", "wiggle",
+  "bubbles", "cage", "connections", "current", "diagonalstripes", "flippeddiamonds",
+  "floatingcogs", "glamorous", "houndstooth", "leaf", "linesinmotion",
+  "moroccan", "morphingdiamonds", "rails", "rain", "skulls", "squaresinsquares",
+  "stripes", "tictactoe", "zigzag", "aztec", "banknote", "boxes", "circlessquares",
+  "circuitboard", "curtain", "diagonallines", "endlessclouds", "eyes",
+  "floortile", "groovy", "intersectingcircles", "melt", "overlappingdiamonds",
+  "parkayfloor", "pixeldots", "polkadots", "signal", "slantedstars", "wallpaper"
+];
+
+
+const colorNames = Object.keys(colors).filter(
+  (name) =>
+    typeof colors[name] === 'object' && // ignore things like 'inherit'
+    !name.startsWith('lightBlue') &&
+    !name.startsWith('warmGray') &&
+    !name.startsWith('trueGray') &&
+    !name.startsWith('coolGray') &&
+    !name.startsWith('blueGray')
+);
+
+const safelist = [
+  // Existing heropattern combinations
+  ...patterns.flatMap(p =>
+    colorNames.flatMap(c => [
+      `heropattern-${p}-${c}-100`,
+      `heropattern-${p}-${c}-200`,
+    ])
+  ),
+
+  // Add all bg-colorname-100 classes
+  ...colorNames.map(c => `bg-${c}-100`),
+];
 
 module.exports = {
   content: [
@@ -52,7 +94,9 @@ module.exports = {
       16: '16',
     }
   },
+    safelist: safelist,
   plugins: [
+    require('tailwindcss-hero-patterns'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/forms'),
