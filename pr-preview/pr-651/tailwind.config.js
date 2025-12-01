ac2ccd760806bc9bb0052b753adce28b41fcb652
HarishV14 <HarishV14@users.noreply.github.com>
2025-12-01 06:24:48 +0000

    Deploy preview for PR 651 🛫

---
+++ pr-preview/pr-651/tailwind.config.js
@@ -0,0 +1,62 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "node_modules/preline/dist/*.js",
+    "node_modules/@themesberg/tailwind-datepicker/dist/js/*.js"
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          50: colors.blue[50],
+          100: colors.blue[100],
+          200: colors.blue[200],
+          300: colors.blue[300],
+          400: colors.blue[400],
+          500: colors.blue[500],
+          600: colors.blue[600],
+          700: colors.blue[700],
+          800: colors.blue[800],
+          900: colors.blue[900],
+        }
+      }
+    },
+    aspectRatio: {
+      auto: 'auto',
+      square: '1 / 1',
+      video: '16 / 9',
+      1: '1',
+      2: '2',
+      3: '3',
+      4: '4',
+      5: '5',
+      6: '6',
+      7: '7',
+      8: '8',
+      9: '9',
+      10: '10',
+      11: '11',
+      12: '12',
+      13: '13',
+      14: '14',
+      15: '15',
+      16: '16',
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('preline/plugin'),
+  ],
+};
