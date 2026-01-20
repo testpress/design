fb81e750a5b6055a34a7406642a9165a394de892
Karthikeyantestpress <Karthikeyantestpress@users.noreply.github.com>
2026-01-20 06:49:01 +0000

    Deploy preview for PR 710 🛫

--- pr-preview/pr-710/tailwind.config.js
+++ pr-preview/pr-710/tailwind.config.js
@@ -17,30 +17,17 @@ module.exports = {
       },
       colors: {
         primary: {
-          50: colors.blue[50],
-          100: colors.blue[100],
-          200: colors.blue[200],
-          300: colors.blue[300],
-          400: colors.blue[400],
-          500: colors.blue[500],
-          600: colors.blue[600],
-          700: colors.blue[700],
-          800: colors.blue[800],
-          900: colors.blue[900],
-        },
-        backgroundDark: {
-          50: colors.neutral[50],
-          100: colors.neutral[100],
-          200: colors.neutral[200],
-          300: colors.neutral[300],
-          400: colors.neutral[400],
-          500: colors.neutral[500],
-          600: colors.neutral[600],
-          700: colors.neutral[700],
-          800: colors.neutral[800],
-          900: colors.neutral[900]
+          50: 'rgb(var(--color-primary-50) / <alpha-value>)',
+          100: 'rgb(var(--color-primary-100) / <alpha-value>)',
+          200: 'rgb(var(--color-primary-200) / <alpha-value>)',
+          300: 'rgb(var(--color-primary-300) / <alpha-value>)',
+          400: 'rgb(var(--color-primary-400) / <alpha-value>)',
+          500: 'rgb(var(--color-primary-500) / <alpha-value>)',
+          600: 'rgb(var(--color-primary-600) / <alpha-value>)',
+          700: 'rgb(var(--color-primary-700) / <alpha-value>)',
+          800: 'rgb(var(--color-primary-800) / <alpha-value>)',
+          900: 'rgb(var(--color-primary-900) / <alpha-value>)',
         }
-        
       }
     },
     aspectRatio: {
