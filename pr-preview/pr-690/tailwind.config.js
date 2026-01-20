21408847ab7a8910f6088dafc9487cbda6a05321
dhinesh-kumar-m <dhinesh-kumar-m@users.noreply.github.com>
2026-01-20 06:01:27 +0000

    Deploy preview for PR 690 🛫

--- pr-preview/pr-690/tailwind.config.js
+++ pr-preview/pr-690/tailwind.config.js
@@ -17,16 +17,16 @@ module.exports = {
       },
       colors: {
         primary: {
-          50: colors.blue[50],
-          100: colors.blue[100],
-          200: colors.blue[200],
-          300: colors.blue[300],
-          400: colors.blue[400],
-          500: colors.blue[500],
-          600: colors.blue[600],
-          700: colors.blue[700],
-          800: colors.blue[800],
-          900: colors.blue[900],
+          50: 'rgb(var(--color-primary-50) / <alpha-value>)',
+          100: 'rgb(var(--color-primary-100) / <alpha-value>)',
+          200: 'rgb(var(--color-primary-200) / <alpha-value>)',
+          300: 'rgb(var(--color-primary-300) / <alpha-value>)',
+          400: 'rgb(var(--color-primary-400) / <alpha-value>)',
+          500: 'rgb(var(--color-primary-500) / <alpha-value>)',
+          600: 'rgb(var(--color-primary-600) / <alpha-value>)',
+          700: 'rgb(var(--color-primary-700) / <alpha-value>)',
+          800: 'rgb(var(--color-primary-800) / <alpha-value>)',
+          900: 'rgb(var(--color-primary-900) / <alpha-value>)',
         }
       }
     },
