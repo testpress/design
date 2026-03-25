027d7ef772099481e6e1e4953da2037e1f921b1b
Bharath <bharath@testpress.in>
2026-03-25 20:21:46 +0530

    feat: add job detail page

--- .eleventy.js
+++ .eleventy.js
@@ -53,6 +53,10 @@ module.exports = config => {
         
         return `Due on ${new Date(dateString).toLocaleDateString('en-US', { month: 'short', day: 'numeric' })}`;
     });
+    config.addFilter("formatDate", (dateString) => {
+        if (!dateString) return "";
+        return new Date(dateString).toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
+    });
     config.addPassthroughCopy({ "src/api": "api" });
     config.addPassthroughCopy("css/simba.css");
     config.addPassthroughCopy("css/odinhire.css");
