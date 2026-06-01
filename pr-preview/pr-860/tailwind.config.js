64ab632af0b3d27e1e66a382cbb38269dfb31495
Karthikeyantestpress <Karthikeyantestpress@users.noreply.github.com>
2026-06-01 08:05:14 +0000

    Deploy preview for PR 860 🛫

--- pr-preview/pr-860/tailwind.config.js
+++ pr-preview/pr-860/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
   content: [
     "./src/**/*.{html,js}",
     "!./src/simba/**",
+    "!./src/sentinel/proctor_widget/**",
     "node_modules/preline/dist/*.js",
     "node_modules/@themesberg/tailwind-datepicker/dist/js/*.js"
   ],
